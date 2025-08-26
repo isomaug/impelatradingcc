4d048e52a20397b7c84936815208dc621071a31b
Isoma <isomaug@gmail.com>
2025-08-26 11:18:35 +0000

    Notification	Suspicious Activity Alert Publicly accessible Google API ke

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "impela-trading-cc",
-  "appId": "1:551174540144:web:85d01388feb0488e0a9db5",
-  "storageBucket": "impela-trading-cc.firebasestorage.app",
-  "apiKey": "AIzaSyAoXgy0SRxyNfqSEgPy-G0YZVX1SIsH5pc",
-  "authDomain": "impela-trading-cc.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "551174540144"
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "storageBucket": process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
 };
 
 // Initialize Firebase
